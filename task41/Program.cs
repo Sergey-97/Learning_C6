1ca29d15bba0697c31a808c3af1027721e3fde30
Sergey <aseev-1977@list.ru>
2023-01-22 21:53:46 +0300

    task43

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Введите числа через запятую: ");
+int[] numbers = StringToNum(Console.ReadLine()!);
+PrintArray(numbers);
+Console.ResetColor ();
+
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write($"Количество чисел больше 0 = ");
+Console.ResetColor ();
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine($" {sum} ");
+Console.ResetColor ();
+
+int[] StringToNum(string natural)
+{
+    int count = 1;
+    for (int i = 0; i < natural.Length; i++)
+    {
+        if (natural[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < natural.Length; i++)
+    {
+        string temp = "";
+
+        while (natural [i] != ',')
+        {
+        if(i != natural.Length - 1)
+        {
+            temp += natural [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += natural [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write("]");
+}
